28859118003769a7f9fbb42e07e571361e397010
redcode <manuel@zxe.io>
2024-02-05 16:02:02 -0500

    Refactor.

--- Z80 XCF Flavor.asm
+++ Z80 XCF Flavor.asm
@@ -1,4 +1,4 @@
-; Z80 XCF Flavor v1.3
+; Z80 XCF Flavor v1.4
 ; Copyright (C) 2022-2024 Manuel Sainz de Baranda y Goñi.
 ;
 ; This program is free software: you can redistribute it and/or modify it under
@@ -60,118 +60,141 @@ start:	di		     ; Disable interrupts.
 	ld   a, 2	     ; Open channel #2 for text output.
 	call OPEN_CHANNEL    ;
 	ld   bc, (UDG)	     ; Save the current UDG pointer.
-	ld   de, udg	     ; Set custom UDG for "ñ".
+	ld   de, nn	     ; Set custom UDG for "ñ".
 	ld   (UDG), de	     ;
-	ld   hl, screen_text ; Print the static text.
+	ld   hl, header_text ; Print the header.
 	call print	     ;
 	ld   (UDG), bc	     ; Restore the original UDG pointer.
 	ld   bc, results     ; Set BC to the address of the results array.
 
-	ld   hl, $1A0D	     ; Configure the <AT><y><x> sequence:
-	ld   (at_yx + 1), hl ;   y = 13; x = 26
-
 	; Test all factor combinations with `ccf` and
-	; print the resulting values of YF and XF.
-	Q0_F0_A0 : ccf : call keep_and_print_yxf
-	Q0_F1_A0 : ccf : call keep_and_print_yxf
-	Q1_F1_A0 : ccf : call keep_and_print_yxf
-	Q0_F0_A1 : ccf : call keep_and_print_yxf
-	Q0_F1_A1 : ccf : call keep_and_print_yxf
-	Q1_F1_A1 : ccf : call keep_and_print_yxf
-
-	ld   hl, $1E0D	     ; Configure the <AT><y><x> sequence:
-	ld   (at_yx + 1), hl ;   y = 13; x = 30
+	; keep the resulting values of YF and XF.
+	Q0_F0_A0 : ccf : call keep_yxf
+	Q0_F1_A0 : ccf : call keep_yxf
+	Q1_F1_A0 : ccf : call keep_yxf
+	Q0_F0_A1 : ccf : call keep_yxf
+	Q0_F1_A1 : ccf : call keep_yxf
+	Q1_F1_A1 : ccf : call keep_yxf
 
 	; Test all factor combinations with `scf` and
-	; print the resulting values of YF and XF.
-	Q0_F0_A0 : scf : call keep_and_print_yxf
-	Q0_F1_A0 : scf : call keep_and_print_yxf
-	Q1_F1_A0 : scf : call keep_and_print_yxf
-	Q0_F0_A1 : scf : call keep_and_print_yxf
-	Q0_F1_A1 : scf : call keep_and_print_yxf
-	Q1_F1_A1 : scf : call keep_and_print_yxf
+	; keep the resulting values of YF and XF.
+	Q0_F0_A0 : scf : call keep_yxf
+	Q0_F1_A0 : scf : call keep_yxf
+	Q1_F1_A0 : scf : call keep_yxf
+	Q0_F0_A1 : scf : call keep_yxf
+	Q0_F1_A1 : scf : call keep_yxf
+	Q1_F1_A1 : scf : call keep_yxf
 
-	ld   hl, $0815		      ; Configure the <AT><y><x> sequence:
-	ld   (at_yx + 1), hl	      ;   y = 21; x = 8
-	ld   hl, at_yx		      ; Print the sequence to move the cursor
-	call print		      ;   after "Result: "
+	ld   c, 6		     ; C = number of rows to print.
+	ld   hl, rows_text	     ; (HL) = Static text of the row.
+	ld   de, results	     ; (DE) = `ccf` results.
+	ld   ix, results + 6	     ; (IX) = `scf` results.
+.print_table_row:
+	call print		     ; Print the static text of the row.
+	ld   a, (de)		     ; Print the results for `ccf` and point DE
+	call print_yxf		     ;   to the next element in the results
+	inc  de			     ;   array.
+	dec  hl			     ; Point HL to the last two spaces in the
+	dec  hl			     ;   static text of the row, and print those
+	call print		     ;   spaces (column gap). Next, point HL to
+	inc  hl			     ;   the static text of the next row.
+	ld   a, (ix)		     ; Print the results for `scf` and point IX
+	call print_yxf		     ;   to the next element in the results
+	inc  ix			     ;   array.
+	dec  c			     ;
+	jr   nz, .print_table_row    ; Repeat until all rows have been printed.
 
-	ld   de, results	      ; Compare the values obtained with `ccf`,
-	ld   hl, results + 6	      ;   against those obtained with `scf`.
-	call compare_results	      ;   They should be the same; otherwise,
-	cp   0			      ;   the behavior is unknown (or unstable)
-	jr   nz, .unknown_flavor      ;   and we report it.
+	call print		     ; Now HL points to the footer; print it.
 
-	ld   de, results	      ; Compare the values obtained with `ccf`
-	ld   hl, results_on_zilog     ;   against the reference values for Zilog
-	call compare_results	      ;   CPU models.
-	ld   hl, zilog_flavor_text    ;
-	cp   0			      ; If the values match, report "Zilog
-	jr   z, .print_result	      ;   flavor" and exit.
+	ld   de, results	     ; Compare the values obtained with `ccf`,
+	ld   hl, results + 6	     ;   against those obtained with `scf`.
+	call compare_results	     ;   They should be the same; otherwise,
+	cp   0			     ;   the behavior is unknown (or unstable)
+	jr   nz, .unknown_flavor     ;   and we report it.
 
-	ld   de, results	      ; Compare the values obtained with `ccf`
-	ld   hl, results_on_nec_nmos  ;   against the reference values for NEC
-	call compare_results	      ;   NMOS CPU models.
-	ld   hl, nec_nmos_flavor_text ;
-	cp   0			      ; If the values match, report "NEC NMOS
-	jr   z, .print_result	      ;   flavor" and exit.
+	ld   de, results	     ; Compare the values obtained with `ccf`
+	ld   hl, results_on_zilog    ;   against the reference values for Zilog
+	call compare_results	     ;   CPU models.
+	ld   hl, zilog_text	     ;
+	cp   0			     ; If the values match, report "Zilog
+	jr   z, .print_result	     ;   flavor" and exit.
 
-	ld   de, results	      ; Compare the values obtained with `ccf`
-	ld   hl, results_on_st_cmos   ;   against the reference values for ST
-	call compare_results	      ;   CMOS CPU models.
-	ld   hl, st_cmos_flavor_text  ;
-	cp   0			      ; If the values match, report "ST CMOS
-	jr   z, .print_result	      ;   flavor" and exit.
+	ld   de, results	     ; Compare the values obtained with `ccf`
+	ld   hl, results_on_nec_nmos ;   against the reference values for NEC
+	call compare_results	     ;   NMOS CPU models.
+	ld   hl, nec_nmos_text	     ;
+	cp   0			     ; If the values match, report "NEC NMOS
+	jr   z, .print_result	     ;   flavor" and exit.
+
+	ld   de, results	     ; Compare the values obtained with `ccf`
+	ld   hl, results_on_st_cmos  ;   against the reference values for ST
+	call compare_results	     ;   CMOS CPU models.
+	ld   hl, st_cmos_text	     ;
+	cp   0			     ; If the values match, report "ST CMOS
+	jr   z, .print_result	     ;   flavor" and exit.
 
 .unknown_flavor:
-	ld   hl, unknown_flavor_text  ; Report "Unknown flavor".
+	ld   hl, unknown_text	     ; Report "Unknown flavor".
 .print_result:
 	call print
-	ei			      ; Re-enable interrupts.
-	ret			      ; Exit to BASIC.
+	ld   hl, flavor_text
+	call print
+	ei			     ; Re-enable interrupts.
+	ret			     ; Exit to BASIC.
 
 
-; Keeps YF and XF into the results array, prints their value at the
-; coordinates specified by the <AT><y><x> sequence stored in `at_yx`
-; and increments <y>.
+; Keeps YF and XF into the results array.
 ;
 ; On entry:
-;   * BC - Address of the element in the results array.
+;   BC - Address of the element in the results array.
 ; On exit:
-;   * BC - Address of the next element in the results array.
+;   BC - Address of the next element in the results array.
+; Destroys:
+;   A and DE.
 
-keep_and_print_yxf:
-	push af		   ; Copy the flags in E and A.
-	pop  de		   ;
-	ld   a, e	   ;
-	and  00101000b	   ; Clear all bits in the copy of A except YF and XF.
-	ld   (bc), a	   ; Keep YF and XF into the results array.
-	inc  bc		   ; Point BC to the next element of the array.
-	ld   hl, at_yx	   ; Move the cursor to the column/row specified by HL
-	call print	   ;   by printing the <AT><y><x> sequence.
-	ld   hl, at_yx + 1 ; Increment <y> in the sequence so that next time we
-	inc  (hl)	   ;   print at the next row of the screen.
-	srl  e		   ; Shift the flags to the right until XF is at bit 0.
-	srl  e		   ;
-	srl  e		   ;
-	ld   d, e	   ; Copy the flags also in D, and shift this register
-	srl  d		   ;   right until YF is at bit 0 (we need to use 2
-	srl  d		   ;   registers because YF is printed first).
-	ld   a, d	   ; Print YF.
-	and  1		   ;
-	add  $30	   ;
-	rst  $10	   ;
-	ld   a, e	   ; Print XF.
-	and  1		   ;
-	add  $30	   ;
-	rst  $10	   ;
+keep_yxf:
+	push af	       ; Transfer F to A.
+	pop  de	       ;
+	ld   a, e      ;
+	and  00101000b ; Clear all flags except YF and XF.
+	ld   (bc), a   ; Keep YF and XF into the results array.
+	inc  bc	       ; Point BC to the next element of the array.
+	ret
+
+
+; Prints YF and XF.
+;
+; On entry:
+;   A - Flags.
+; Destroys:
+;   A and B.
+
+print_yxf:
+	ld   b, a ; Copy the flags to B.
+	srl  b	  ; Shift the flags to the right until XF is at bit 0.
+	srl  b	  ;
+	srl  b	  ;
+	ld   a, b ; Copy the shifted flags back to A, and shift this register to
+	srl  a	  ;   the right until YF is at bit 0 (we need to use 2 registers
+	srl  a	  ;   because YF is printed first).
+	and  1	  ; Clear all bits except bit 0.
+	add  $30  ; Translate the value of YF to ASCII.
+	rst  $10  ; Print the value of YF.
+	ld   a, b ; Copy B to A. Now bit 0 of A contains XF.
+	and  1	  ; Clear all bits except bit 0.
+	add  $30  ; Translate the value of XF to ASCII.
+	rst  $10  ; Print the value of XF.
 	ret
 
 
 ; Prints a 1Fh-terminated string.
 ;
 ; On entry:
-;   * HL - String address.
+;   HL - String address.
+; On exit:
+;   HL - Address of the termination byte.
+; Destroys:
+;   A.
 
 print:	ld   a, (hl)
 	cp   $1F
@@ -184,10 +207,12 @@ print:	ld   a, (hl)
 ; Compares 2 arrays of results.
 ;
 ; On entry:
-;   * HL - Array 1 address.
-;   * DE - Array 2 address.
+;   HL - Array 1 address.
+;   DE - Array 2 address.
 ; On exit:
-;   * A - 0 if the arrays are equal; otherwise, a non-zero value.
+;   A - 0 if the arrays are equal; otherwise, a non-zero value.
+; Destroys:
+;   C, DE and HL.
 
 compare_results:
 	ld   c, 6
@@ -210,14 +235,12 @@ results_on_nec_nmos:
 	db 00000000b, 00000000b, 00000000b, 00101000b, 00101000b, 00101000b
 results_on_st_cmos:
 	db 00000000b, 00100000b, 00000000b, 00101000b, 00101000b, 00101000b
-at_yx:
-	db 22, 13, 26, $1F
-screen_text:
-	db "Z80 XCF "
-	db 19, 1, 17, 2, 16, 7, "F", 17, 3, "L", 17, 1, "A", 17, 5, 16, 8, "V"
-	db 17, 4, "O", 17, 6, "R", 17, 8, 16, 8, 19, 8, " v1.3 (",__DATE__,")\r"
-	db 127, " Manuel Sainz de Baranda y Go", $90, "i\r"
-	db 16, 1, "https://zxe.io", 16, 8, "\r"
+header_text:
+	db 'Z80 XCF '
+	db 19, 1, 17, 2, 16, 7, 'F', 17, 3, 'L', 17, 1, 'A', 17, 5, 16, 8, 'V'
+	db 17, 4, 'O', 17, 6, 'R', 17, 8, 16, 8, 19, 8, ' v1.4 (',__DATE__,")\r"
+	db 127, ' Manuel Sainz de Baranda y Go', $90, "i\r"
+	db 16, 1, 'https://zxe.io', 16, 8, "\r"
 	db "\r"
 	db "This program checks the behavior\r"
 	db "of the undocumented flags during\r"
@@ -227,25 +250,29 @@ screen_text:
 	db "\r"
 	db 19, 1, 17, 0, 16, 7,"  Case    Any  NEC   ST    HOST \r"
 	db " Tested  Zilog NMOS CMOS   CPU  \r"
-	db "(Q<>F)|A   YX   YX   YX   YX  YX", 17, 8, 16, 8, 19, 8, "\r"
-	db "(0<>0)|0   00   00   00\r"
-	db "(0<>1)|0   11   00   10\r"
-	db "(1<>1)|0   00   00   00\r"
-	db "(0<>0)|1   11   11   11\r"
-	db "(0<>1)|1   11   11   11\r"
-	db "(1<>1)|1   11   11   11\r"
-	db 17, 0, 16, 7, 23, 25, 0, 19, 1, "ccf scf", 17, 8, 16, 8, 19, 8, "\r"
+	db '(Q<>F)|A   YX   YX   YX   YX  YX', 17, 8, 16, 8, 19, 8, $1F
+rows_text:
+	db "\r(0<>0)|0   00   00   00   ", $1F
+	db "\r(0<>1)|0   11   00   10   ", $1F
+	db "\r(1<>1)|0   00   00   00   ", $1F
+	db "\r(0<>0)|1   11   11   11   ", $1F
+	db "\r(0<>1)|1   11   11   11   ", $1F
+	db "\r(1<>1)|1   11   11   11   ", $1F
+footer_text:
+	db 17, 0, 16, 7, 23, 25, 0, 19, 1, 'ccf scf', 17, 8, 16, 8, 19, 8, "\r"
 	db "\r"
-	db "Result:",$1F
-zilog_flavor_text:
-	db 19, 1, 17, 4, "Zilog", 19, 8, 17, 8, " flavor", $1F
-nec_nmos_flavor_text:
-	db 19, 1, 17, 4, "NEC NMOS", 19, 8, 17, 8, " flavor", $1F
-st_cmos_flavor_text:
-	db 19, 1, 17, 4, "ST CMOS", 19, 8, 17, 8, " flavor", $1F
-unknown_flavor_text:
-	db 19, 1, 18, 1, 17, 2, "Unknown", 19, 8, 18, 8, 17, 8, " flavor", $1F
-udg:
+	db 'Result: ', $1F
+zilog_text:
+	db 19, 1, 17, 4, 'Zilog', $1F
+nec_nmos_text:
+	db 19, 1, 17, 4, 'NEC NMOS', $1F
+st_cmos_text:
+	db 19, 1, 17, 4, 'ST CMOS', $1F
+unknown_text:
+	db 19, 1, 17, 2, 18, 1, 'Unknown', 18, 8, $1F
+flavor_text:
+	db 17, 8, 19, 8, ' flavor', $1F
+nn:
 	db 00000000b ; ñ
 	db 00111000b
 	db 00000000b
@@ -255,8 +282,10 @@ udg:
 	db 01000100b
 	db 00000000b
 
+PROGRAM_SIZE = $ - start
 
-	SAVESNA "Z80 XCF Flavor.sna", start
+
+	savesna 'Z80 XCF Flavor.sna', start
 
 
 CLEAR	  = $FD
@@ -265,36 +294,33 @@ LOAD	  = $EF
 RANDOMIZE = $F9
 USR	  = $C0
 
-	macro MAKE_TAPE tape_file, prog_name, start_add, code_len, call_add
-
 	org $5C00
 basic:	db  0, 1
-	dw  line_1_size
+	dw  LINE_1_SIZE
 line_1:	db  CLEAR, '8', $0E, 0, 0
-	dw  start_add - 1
+	dw  start - 1
 	db  0, ':'
 	db  LOAD, '"'
 name:	ds  10, 32
 	org name
-	db  prog_name
+	db  'XCF Flavor'
 	org name + 10
 	db  '"', CODE, $0D
-line_1_size = $ - line_1
+LINE_1_SIZE = $ - line_1
 	db  0, 2
-	dw  line_2_size
+	dw  LINE_2_SIZE
 line_2:	db  RANDOMIZE, USR, '8', $0E, 0, 0
-	dw  call_add
+	dw  start
 	db  0, $0D
-line_2_size = $ - line_2
-basic_size  = $ - basic
+LINE_2_SIZE = $ - line_2
+BASIC_SIZE  = $ - basic
 
-	EMPTYTAP tape_file
-	SAVETAP  tape_file, BASIC, prog_name, basic, basic_size, 1
-	SAVETAP  tape_file, CODE, prog_name, start_add, code_len, start_add
-	ENDM
+	macro MAKE_TAPE tap_name, file_name
+		emptytap tap_name
+		savetap  tap_name, BASIC, file_name, basic, BASIC_SIZE, 1
+		savetap  tap_name, CODE, file_name, start, PROGRAM_SIZE, start
+	endm
 
-size = $ - start
-
-	MAKE_TAPE "Z80 XCF Flavor.tap", "Z80 XCF", start, size, start
+	MAKE_TAPE 'Z80 XCF Flavor.tap', 'XCF Flavor'
 
 ; EOF
